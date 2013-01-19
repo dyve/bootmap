c36d8c35f61195cd9bad6bfb6538913a494711d2
Dylan Verheul <dylan@zostera.nl>
2013-01-19 13:59:00 +0100

    QUnit testing in place, first requirements set

---
+++ js/bootmap.js
@@ -0,0 +1,16 @@
+/**
+ * @file bootmap.js
+ * @author Dylan Verheul <dylan@dyve.net>
+ * @license See LICENSE.txt
+ */
+(function($) {
+
+    var bootmap = window.bootmap = {};
+
+    bootmap.init = function() {
+
+    };
+
+    bootmap.init();
+
+})(window.bootmap$ || window.jQuery);

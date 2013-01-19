bb35e53b31fe4464a7d862e16b28b9fdc3634d1b
Dylan Verheul <dylan@zostera.nl>
2013-01-19 16:58:33 +0100

    WIP

--- js/bootmap.js
+++ js/bootmap.js
@@ -5,12 +5,25 @@
  */
 (function($) {
 
+    /**
+     * Globally available window.bootmap object to serve as namespace
+     * @namespace bootmap
+     */
     var bootmap = window.bootmap = {};
 
+    /**
+     * Function that is run on page ready by jQuery
+     */
     bootmap.init = function() {
-
+        $("[data-role='map-canvas']").each(function() {
+            var $mapCanvas = $(this);
+            $mapCanvas.data('bootmap', true);
+        });
     };
 
+    /**
+     * Run bootmap.init on page ready
+     */
     bootmap.init();
 
 })(window.bootmap$ || window.jQuery);
